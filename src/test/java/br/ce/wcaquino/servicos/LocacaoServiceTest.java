8b5f01c97bfe6b071ea5b10d7b376652137699b3
Toshiro <toshiro.lyn@gmail.com>
2020-10-12 14:48:57 -0300

    Utilizando ErrorCollector / Estratégias de Testes

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -3,13 +3,12 @@ package br.ce.wcaquino.servicos;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -17,9 +16,12 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
 
 	@Test
-	public void teste() {
+	public void testeLocacao() {
 		// cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
@@ -30,9 +32,16 @@ public class LocacaoServiceTest {
 
 		// verificacao
 		//(1º-valor esperado, 2º Valor recebido)
-		assertThat(locacao.getValor() ,is(equalTo(5.0)));
-		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(locacao.getValor() ,is(equalTo(5.0)));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		
+		/*
+		 * Existem diversas formas de lidar com testes, algumas pessoas defendem que cada assert deverá estar em uma classe diferente, já que quando colocamos todos em uma só, como agora, 
+		 * é apenas sinalizado o 1º erro, caso tenham outros, só serão mostrado após a sua correção e assim sucessivamente. Só que ao utilizar essa estratégias, sserão geradas diversas classes para cada teste.
+		 * Aqui iremos utilizar a classe ErrorCollector, que irá sinalizar todos os erros de uma só vez e irá também informar aonde estão esses erros, para que assim possamos verificar vários erros dentro da mesma classe.
+		 * Cada estratégia possui seus prós e contras, devendo ser analizado caso a caso.
+		 * */
 	}
 
 }
